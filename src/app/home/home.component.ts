2033b7ab5f7ed913383d21f6df11a76a1413e5fd
rtyx <rafaeltoledanoillan@gmail.com>
2019-12-03 10:45:16 +0100

    deployed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title) {
+  }
 
   ngOnInit() {
+    this.title.setTitle('RafaelToledanoIllan');
+    console.log(`%c
+ _____ _____ __ __ __ __
+| __  |_   _|  |  |  |  |
+|    -| | | |_   _|-   -|
+|__|__| |_|   |_| |__|__|
+    `, 'color: rgb(145, 121, 157); font-weight: bold;');
+    console.log(`Hi there! Nice to see you here.`);
+    console.log(`As most things on the web, this is a work in progress. If you'd like to see some of my work, please visit some of the link on the left.`);
+    console.log(`Thanks!`);
+    console.log('');
+    console.log('https://www.rtyx.es');
   }
 
 }
